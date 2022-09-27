3ed7e56d6502a5e49eeec93fba6a4669aa54123f
Huynh Nguyen Hieu Nhan <hieunhantv2002@gmail.com>
2022-09-27 15:44:07 -0800

    bai 5

--- code/Core/Inc/main.h
+++ code/Core/Inc/main.h
@@ -58,20 +58,46 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define A_Pin GPIO_PIN_1
-#define A_GPIO_Port GPIOA
-#define B_Pin GPIO_PIN_2
-#define B_GPIO_Port GPIOA
-#define C_Pin GPIO_PIN_3
-#define C_GPIO_Port GPIOA
-#define D_Pin GPIO_PIN_4
-#define D_GPIO_Port GPIOA
-#define E_Pin GPIO_PIN_5
-#define E_GPIO_Port GPIOA
-#define F_Pin GPIO_PIN_6
-#define F_GPIO_Port GPIOA
-#define G_Pin GPIO_PIN_7
-#define G_GPIO_Port GPIOA
+#define LED_RED_1_Pin GPIO_PIN_1
+#define LED_RED_1_GPIO_Port GPIOA
+#define LED_YELLOW_1_Pin GPIO_PIN_2
+#define LED_YELLOW_1_GPIO_Port GPIOA
+#define LED_GREEN_1_Pin GPIO_PIN_3
+#define LED_GREEN_1_GPIO_Port GPIOA
+#define LED_RED_2_Pin GPIO_PIN_4
+#define LED_RED_2_GPIO_Port GPIOA
+#define LED_YELLOW_2_Pin GPIO_PIN_5
+#define LED_YELLOW_2_GPIO_Port GPIOA
+#define LED_GREEN_2_Pin GPIO_PIN_6
+#define LED_GREEN_2_GPIO_Port GPIOA
+#define A1_Pin GPIO_PIN_0
+#define A1_GPIO_Port GPIOB
+#define B1_Pin GPIO_PIN_1
+#define B1_GPIO_Port GPIOB
+#define C1_Pin GPIO_PIN_2
+#define C1_GPIO_Port GPIOB
+#define D2_Pin GPIO_PIN_10
+#define D2_GPIO_Port GPIOB
+#define E2_Pin GPIO_PIN_11
+#define E2_GPIO_Port GPIOB
+#define F2_Pin GPIO_PIN_12
+#define F2_GPIO_Port GPIOB
+#define G2_Pin GPIO_PIN_13
+#define G2_GPIO_Port GPIOB
+#define D1_Pin GPIO_PIN_3
+#define D1_GPIO_Port GPIOB
+#define E1_Pin GPIO_PIN_4
+#define E1_GPIO_Port GPIOB
+#define F1_Pin GPIO_PIN_5
+#define F1_GPIO_Port GPIOB
+#define G1_Pin GPIO_PIN_6
+#define G1_GPIO_Port GPIOB
+#define A2_Pin GPIO_PIN_7
+#define A2_GPIO_Port GPIOB
+#define B2_Pin GPIO_PIN_8
+#define B2_GPIO_Port GPIOB
+#define C2_Pin GPIO_PIN_9
+#define C2_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
